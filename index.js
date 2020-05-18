1f6222f55a91fca85760d27c9c230770d3bae812
samdias97 <48606334+samdias97@users.noreply.github.com>
2020-05-18 14:31:42 -0300

    Tipos de Resposta

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 
-app.all('/opa',(req, res) => {
-    res.send('Estou <b>bem</b>!')
+app.get('/opa',(req, res) => {
+    res.json({
+        data: [
+            {id: 7, name: 'Ana', position: 1},
+            {id: 34, name: 'Bia', position: 2},
+            {id: 73, name: 'Carlos', position: 3}
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
+    /* res.json({
+        name: 'iPad 32Gb',
+        price: 1899.00,
+        discount: 0.12
+    }) */
+   //res.send('<h1>Estou bem!</h1>')
 })
 
 app.listen(3000, () => {
