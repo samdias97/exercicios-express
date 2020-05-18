f7e211271449ed6b4a872e6196035b6d205e8cbd
samdias97 <48606334+samdias97@users.noreply.github.com>
2020-05-18 16:04:57 -0300

    Comunicação Entre Módulos #02

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const saudacao = require('./saudacaoMid')
+const usuarioApi = require('./api/usuario')
+const produtoApi = require('./api/produto')
+produtoApi(app, 'com param!')
+
+app.post('/usuario', usuarioApi.salvar)
+app.get('/usuario', usuarioApi.obter)
 
 app.use(bodyParser.text())
 app.use(bodyParser.json())
