ef17b46392c83a58495327b87fa88b3ff9373ff6
samdias97 <48606334+samdias97@users.noreply.github.com>
2020-05-18 14:20:39 -0300

    Tratando a Primeira Requisição

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.all('/opa',(req, res) => {
+    res.send('Estou <b>bem</b>!')
+})
+
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
