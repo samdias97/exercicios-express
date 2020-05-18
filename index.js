b13696b15ff1ebb889708231af1af5d50341bc49
samdias97 <48606334+samdias97@users.noreply.github.com>
2020-05-18 14:54:23 -0300

    Middleware #02

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+const saudacao = require('./saudacaoMid')
+
+app.use(saudacao('Guilherme'))
+
 app.use((req, res, next) => {
     console.log('Antes...')
     next()
