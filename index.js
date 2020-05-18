b2f3224a5034dc9ee2c4f979ccc0a9f2e14c8074
samdias97 <48606334+samdias97@users.noreply.github.com>
2020-05-18 14:46:09 -0300

    Middleware #01

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/opa',(req, res) => {
+app.use((req, res, next) => {
+    console.log('Antes...')
+    next()
+})
+
+app.get('/opa',(req, res, next) => {
+    console.log('Durante...')
     res.json({
         data: [
             {id: 7, name: 'Ana', position: 1},
@@ -13,12 +19,19 @@ app.get('/opa',(req, res) => {
         limit: 3,
         status: 200
     })
+
+    next()
+
     /* res.json({
         name: 'iPad 32Gb',
         price: 1899.00,
         discount: 0.12
     }) */
    //res.send('<h1>Estou bem!</h1>')
+})
+
+app.use((req, res) => {
+    console.log('Depois...')
 })
 
 app.listen(3000, () => {
